7ddfe706d55fe9f9c97da0b78d978f3bcf2bd904
2018JTM2243 <devendra1012khatri@gmail.com>
2018-09-27 16:18:52 +0530

    Input code ready

--- ps2.py
+++ ps2.py
@@ -1,3 +1,13 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+import sys
+
+k1=int(sys.argv[1])
+k2=int(sys.argv[2]) #commond line input
+k3=int(sys.argv[3])
+if k1>151 or k1<2 or k2>151 or k2<2 or k3>151 or k3<2: # Boundary condition
+    print("Invalid input")
+    exit()
+str=input("enter string")
+print(str)
