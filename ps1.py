1fbee9251c3d8cd8dc67804d78718c47e66f9343
2018JTM2243 <devendra1012khatri@gmail.com>
2018-09-27 14:37:25 +0530

    Array of lists formed

--- ps1.py
+++ ps1.py
@@ -1,3 +1,21 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+import sys                      #include system module
+n=int(sys.argv[1])              #For commond line argument
+m=int(sys.argv[2])
+print(m)                        #check the inputs come
+print(n)
+lists=[]
+if m>106 or m<3 :               #Input condition
+    print("invalid m value")
+
+if n>106 or n<3  :
+    print("Invalid n value")
+s=0
+a=[]
+for i in range(n):
+    print(i)              #create a array of string
+    lists.append(input().split())
+
+print(lists)
